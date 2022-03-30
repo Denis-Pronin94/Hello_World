3761d251bc8c02c4dd10f396dcf32cdc95bd1a48
dddpr <ddd.pronin@yandex.ru>
2022-03-30 23:08:54 +0300

    Another

--- src/qa/denis/Main.java
+++ src/qa/denis/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, WORLD");
-        System.out.println("Another line");
+        System.out.println("Another line!");
         System.out.println("slow branch");
     }
     
     public void another() {
-        System.out.println("ANOTHER METHOD");
+        System.out.println("Another method");
 }
